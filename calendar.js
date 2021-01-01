768a4ff58d3a7da3a4fcc63877c26e4a24d58726
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2021-01-01 09:25:20 -0800

    Created calendar layout

--- calendar.js
+++ calendar.js
@@ -1,14 +1,57 @@
 $(function() {
-    var send = '<div>';
-    dates = testing();
-    dates.forEach(async function(date) {
-        send += '<div class="row">' + date + '</div>';
-    })
-    $("body").append(send);
+    //dates.forEach(async function(date) {
+    //    send += '<div>' + date + '</div>';
+    //})
+    $("body").append(testing(getDates()));
 })
 
-function testing() {
-    var dates = []
+function testing(dates) {
+    times = getTime();
+    var calendar = '<div>';
+    for (var time = 0; time < 25; time++) {
+        calendar += '<div class="row">'
+        for  (var day = 0; day < 8; day++) {
+            if (time == 0) {
+                var date = dates[day]
+                if (day != 0) {
+                    calendar += '<div class="col-sm date">'
+                        + date.getDate()
+                        + ' '
+                        + date.toLocaleDateString("en-US",{ weekday: 'long' })
+                        + '</div>'
+                }
+                else {
+                    calendar += '<div class="col-sm slot"></div>'
+                }
+            }
+            else if (day == 0) {
+                calendar += '<div class="col-sm time">' + times[time] + '</div>'
+            }
+            else {
+                calendar += '<div class="col-sm slot">content</div>'
+            }
+        }
+        calendar += '</div>'
+    }
+    return calendar
+}
+
+function getTime() {
+    var times = [null];
+    for (var i = 0; i < 24; i++) {
+        var time = i % 12 + 1;
+        if (i < 12 ) {
+            times.push(time + " AM");
+        }
+        else {
+            times.push(time + " PM");
+        }
+    }
+    return times
+}
+
+function getDates() {
+    var dates = [null]
     var date = new Date();
     var day = date.getDate();
     var month = date.getMonth();
@@ -36,7 +79,8 @@ function testing() {
             day -= 7;
             break;
     }
-    for (i = 0; i < 21; i++) {
+    day += 7
+    for (i = 0; i < 7; i++) {
         dates.push(new Date(year, month, day + i))
     }
     return dates
