234b90703d56dcc8167ad39e7bc4186c2fb1a3a5
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2021-01-11 11:21:07 -0800

    Added functionality to Settings

--- js/calendar.js
+++ js/calendar.js
@@ -1,41 +1,155 @@
 $(function() {
     document.getElementById('settings').selectedIndex = parseInt(localStorage.getItem('option'))
-    $("body").append(calendar(getDates()));
-    shows()
+    TheBigBang();
     $("#nav").load("nav.html");
 })
 
 $(document).ready(function() {
     $('#left').click(function() {
-        $('#calendar').remove();
         if ($("#right")[0].style.display == "") {
             $("#left").hide();
-            $("body").append(calendar(getDates(-7)));
+            TheBigBang(-7);
         }
         else {
             $("#right").show();
             $("#left").show();
-            $("body").append(calendar(getDates()));
+            TheBigBang();
         }
-        shows()
     })
     $('#right').click(function() {
-        $('#calendar').remove();
         if ($("#left")[0].style.display == "") {
             $("#right").hide();
-            $("body").append(calendar(getDates(+7)));
+            TheBigBang(+7);
         }
         else {
             $("#right").show();
             $("#left").show();
-            $("body").append(calendar(getDates()));
+            TheBigBang();
         }
-        shows()
     })
     $('select').change(function() {
         localStorage.setItem('option', $(this)[0].value)
+        TheBigBang();
     })
 })
+
+function TheBigBang(offset) {
+    $('#calendar').remove();
+    var file
+    switch(localStorage.getItem('option')) {
+        case "1":
+            file = "./shows/cutoff.json"
+            break;
+        case "2":
+            file = "./shows/compact.json"
+            break;
+        default:
+            file = "./shows/full.json"
+    }
+    fetch(file)
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(function(data) {
+            $("body").append(calendar(getDates(offset), data));
+        })
+    shows()
+}
+
+function calendar(dates, times) {
+    var calendar = '<div id="calendar" class="container-fluid">';
+    times.forEach(async function(time) {
+        calendar += '<div class="row"><div class="col-2"></div>'
+        for  (var day = 0; day < 8; day++) {
+            var date = dates[day]
+            if (time == null) {
+                if (day != 0) {
+                    calendar += '<div class="col date">'
+                        + date.getDate()
+                        + ' '
+                        + date.toLocaleDateString("en-US",{ weekday: 'long' })
+                        + '</div>'
+                }
+                else {
+                    calendar += '<div class="col slot"></div>'
+                }
+            }
+            else if (day == 0) {
+                calendar += '<div class="col time">' + time + '</div>'
+            }
+            else {
+                calendar += '<div class="col slot" id="'
+                    + ider(date.toLocaleDateString("en-US",{ weekday: 'long' }), time)
+                    + '"></div>'
+            }
+            if (day == 7) {
+                calendar += '<div class="col-2"></div>'
+            }
+        }
+        calendar += '</div>'
+    })
+    var element = document.getElementById('month');
+    if (dates[1].getMonth() == dates[7].getMonth()) {
+        element.innerHTML = dates[1].toLocaleDateString("en-US",{ month: 'long' })
+    }
+    else {
+        element.innerHTML = dates[1].toLocaleDateString("en-US",{ month: 'long' })
+            + " to "
+            + dates[7].toLocaleDateString("en-US",{ month: 'long' })
+    }
+    return calendar
+}
+
+function getDates(offset = 0) {
+    var dates = [null]
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth();
+    var year = date.getFullYear();
+    switch(date.getDay()) {
+        case 0:
+            day += offset - 6;
+            break;
+        case 1:
+            day += offset;
+            break;
+        case 2:
+            day += offset - 1;
+            break;
+        case 3:
+            day += offset - 2;
+            break;
+        case 4:
+            day += offset - 3;
+            break;
+        case 5:
+            day += offset - 4;
+            break;
+        case 6:
+            day += offset - 5;
+            break;
+    }
+    for (i = 0; i < 7; i++) {
+        dates.push(new Date(year, month, day + i))
+    }
+    return dates
+}
+
+function ider(day, time) {
+    const days = {Monday:1, Tuesday:2, Wednesday:3, Thursday:4, Friday:5, Saturday:6, Sunday:7}
+    var id = days[day] * 10000
+    if (time.substring(1,2) == ":") {
+        id += parseInt(time.substring(0,1)) * 100
+        id += parseInt(time.substring(2,4))
+        id = id.toString() + time.substring(5,6)
+    }
+    else {
+        id += parseInt(time.substring(0,2)) * 100
+        id += parseInt(time.substring(3,5))
+        id = id.toString() + time.substring(6,7)
+    }
+    return id
+}
 
 function shows() {
     fetch("./shows/shows.json")
@@ -116,114 +230,4 @@ function shows() {
         });
 }
 
-function calendar(dates) {
-    times = getTime();
-    var calendar = '<div id="calendar" class="container-fluid">';
-    times.forEach(async function(time) {
-        calendar += '<div class="row"><div class="col-2"></div>'
-        for  (var day = 0; day < 8; day++) {
-            var date = dates[day]
-            if (time == null) {
-                if (day != 0) {
-                    calendar += '<div class="col date">'
-                        + date.getDate()
-                        + ' '
-                        + date.toLocaleDateString("en-US",{ weekday: 'long' })
-                        + '</div>'
-                }
-                else {
-                    calendar += '<div class="col slot"></div>'
-                }
-            }
-            else if (day == 0) {
-                calendar += '<div class="col time">' + time + '</div>'
-            }
-            else {
-                calendar += '<div class="col slot" id="'
-                    + ider(date.toLocaleDateString("en-US",{ weekday: 'long' }), time)
-                    + '"></div>'
-            }
-            if (day == 7) {
-                calendar += '<div class="col-2"></div>'
-            }
-        }
-        calendar += '</div>'
-    })
-    var element = document.getElementById('month');
-    if (dates[1].getMonth() == dates[7].getMonth()) {
-        element.innerHTML = dates[1].toLocaleDateString("en-US",{ month: 'long' })
-    }
-    else {
-        element.innerHTML = dates[1].toLocaleDateString("en-US",{ month: 'long' })
-            + " to "
-            + dates[7].toLocaleDateString("en-US",{ month: 'long' })
-    }
-    return calendar
-}
 
-function getTime() {
-    var times = [null];
-    for (var i = 11; i < 35; i++) {
-        var time = i % 12 + 1;
-        if (i < 23 ) {
-            times.push(time + ":00 AM");
-            times.push(time + ":30 AM");
-        }
-        else {
-            times.push(time + ":00 PM");
-            times.push(time + ":30 PM");
-        }
-    }
-    return times
-}
-
-function getDates(offset = 0) {
-    var dates = [null]
-    var date = new Date();
-    var day = date.getDate();
-    var month = date.getMonth();
-    var year = date.getFullYear();
-    switch(date.getDay()) {
-        case 0:
-            day += offset - 6;
-            break;
-        case 1:
-            day += offset;
-            break;
-        case 2:
-            day += offset - 1;
-            break;
-        case 3:
-            day += offset - 2;
-            break;
-        case 4:
-            day += offset - 3;
-            break;
-        case 5:
-            day += offset - 4;
-            break;
-        case 6:
-            day += offset - 5;
-            break;
-    }
-    for (i = 0; i < 7; i++) {
-        dates.push(new Date(year, month, day + i))
-    }
-    return dates
-}
-
-function ider(day, time) {
-    const days = {Monday:1, Tuesday:2, Wednesday:3, Thursday:4, Friday:5, Saturday:6, Sunday:7}
-    var id = days[day] * 10000
-    if (time.substring(1,2) == ":") {
-        id += parseInt(time.substring(0,1)) * 100
-        id += parseInt(time.substring(2,4))
-        id = id.toString() + time.substring(5,6)
-    }
-    else {
-        id += parseInt(time.substring(0,2)) * 100
-        id += parseInt(time.substring(3,5))
-        id = id.toString() + time.substring(6,7)
-    }
-    return id
-}
