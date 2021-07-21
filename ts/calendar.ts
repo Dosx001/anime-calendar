cd7b9d2e01c63df49b3daa81e3f1e1c85427acd7
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2021-07-21 08:35:16 -0700

    Fixed 'The Honor at Magic High School' bug

--- ts/calendar.ts
+++ ts/calendar.ts
@@ -9,7 +9,7 @@ $(function() {
     if (localStorage.getItem('shows') == null) {
         localStorage.setItem('shows', JSON.stringify({}))
     }
-    let VERSION = "21.2.0"
+    let VERSION = "21.2.1"
     if (localStorage.getItem('ver') != VERSION) {
         localStorage.setItem('ver', VERSION)
         set("./shows/shows.json", "storage")
