0506a167a2a8dd941f904fc3f2bd38862ac15a2f
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2021-08-05 06:58:54 -0700

    Added the ability to move through search results

--- js/search.js
+++ js/search.js
@@ -2,34 +2,68 @@
 let titleList = Object.keys(Object.assign(JSON.parse(localStorage.getItem("storage")), JSON.parse(localStorage.getItem("past"))));
 const search = document.getElementById('search');
 const titles = document.getElementById('titles');
+let indexLi = 99;
 search.onkeyup = (e) => {
-    switch (e.keyCode) {
-        case 38:
-            console.log('up');
-            break;
-        case 40:
-            console.log('down');
-            break;
-        default:
-            let input = e.target.value;
-            if (input) {
-                titles.style.display = "";
-                titles.innerHTML = "";
-                for (let i in titleList) {
-                    if (titleList[i].toLocaleLowerCase().includes(input.toLocaleLowerCase())) {
-                        let li = document.createElement('li');
-                        li.innerHTML = titleList[i];
-                        li.classList.add('highlight');
-                        li.setAttribute('id', (i + 100).toString());
-                        li.addEventListener('click', function () {
-                            console.log(this.innerHTML);
-                        });
-                        titles.append(li);
-                    }
+    if (e.key.length == 1 || e.key == "Backspace" || e.key == "Delete") {
+        let input = e.target.value;
+        indexLi = 99;
+        if (input) {
+            titles.style.display = "";
+            titles.innerHTML = "";
+            let i = 100;
+            for (let title of titleList) {
+                if (title.toLocaleLowerCase().includes(input.toLocaleLowerCase())) {
+                    let li = document.createElement('li');
+                    li.innerHTML = title;
+                    li.classList.add('highlight');
+                    li.setAttribute('id', i.toString());
+                    li.setAttribute('tabindex', (i - 100).toString());
+                    i++;
+                    li.addEventListener('click', function () {
+                        console.log(this.innerHTML);
+                    });
+                    titles.append(li);
                 }
             }
-            else {
+            if (i == 100) {
                 titles.style.display = "none";
             }
+        }
+        else {
+            titles.style.display = "none";
+        }
     }
 };
+search.onkeydown = (e) => {
+    switch (e.keyCode) {
+        case 38:
+            move(-1);
+            e.preventDefault();
+            break;
+        case 40:
+            move(1);
+            e.preventDefault();
+            break;
+    }
+};
+function move(num) {
+    let mark = document.getElementById((indexLi + num).toString());
+    if (mark) {
+        mark.classList.add('active');
+        mark.focus();
+        search.focus();
+        if (mark.nextElementSibling) {
+            mark.nextElementSibling.classList.remove('active');
+        }
+        if (mark.previousElementSibling) {
+            mark.previousElementSibling.classList.remove('active');
+        }
+        if (document.getElementById((indexLi + num).toString())) {
+            indexLi += num;
+        }
+    }
+}
+search.addEventListener('focusout', (e) => {
+    titles.style.display = (e.relatedTarget) ?
+        ((e.relatedTarget.classList[1] == "active") ? "" : "none") : "none";
+});
--- ts/search.ts
+++ ts/search.ts
@@ -2,36 +2,72 @@ let titleList = Object.keys(Object.assign(JSON.parse(localStorage.getItem("stora
                                            ,JSON.parse(localStorage.getItem("past")!)))
 const search = document.getElementById('search')!
 const titles = document.getElementById('titles')!
-//let index = 100
+let indexLi = 99
 
 search.onkeyup = (e) => {
-    switch(e.keyCode) {
-        case 38: //up
-            console.log('up')
-            break
-        case 40: //down
-            console.log('down')
-            break
-        default:
-            let input = (<HTMLInputElement>e.target!).value
-            if(input){
-                titles.style.display = ""
-                titles.innerHTML = ""
-                for (let i in titleList) {
-                    if (titleList[i].toLocaleLowerCase().includes(input.toLocaleLowerCase())) {
-                        let li = document.createElement('li')
-                        li.innerHTML = titleList[i]
-                        li.classList.add('highlight')
-                        li.setAttribute('id', (i + 100).toString())
-                        li.addEventListener('click', function() {
-                            console.log(this.innerHTML)
-                        })
-                        titles.append(li)
-                    }
+    if (e.key.length == 1 || e.key == "Backspace" || e.key == "Delete") {
+        let input = (<HTMLInputElement>e.target!).value
+        indexLi = 99
+        if(input){
+            titles.style.display = ""
+            titles.innerHTML = ""
+            let i = 100
+            for (let title of titleList) {
+                if (title.toLocaleLowerCase().includes(input.toLocaleLowerCase())) {
+                    let li = document.createElement('li')
+                    li.innerHTML = title
+                    li.classList.add('highlight')
+                    li.setAttribute('id', i.toString())
+                    li.setAttribute('tabindex', (i - 100).toString())
+                    i++
+                    li.addEventListener('click', function() {
+                        console.log(this.innerHTML)
+                    })
+                    titles.append(li)
                 }
             }
-            else {
+            if (i == 100) {
                 titles.style.display = "none"
             }
+        }
+        else {
+            titles.style.display = "none"
+        }
     }
 }
+
+search.onkeydown = (e) => {
+    switch(e.keyCode) {
+        case 38: //up
+            move(-1)
+            e.preventDefault();
+            break
+        case 40: //down
+            move(1)
+            e.preventDefault();
+            break
+    }
+}
+
+function move(num: number) {
+    let mark = document.getElementById((indexLi + num).toString())!
+    if (mark) {
+        mark.classList.add('active')
+        mark.focus()
+        search.focus()
+        if (mark.nextElementSibling) {
+            mark.nextElementSibling.classList.remove('active')
+        }
+        if (mark.previousElementSibling) {
+            mark.previousElementSibling.classList.remove('active')
+        }
+        if (document.getElementById((indexLi + num).toString())) {
+            indexLi += num
+        }
+    }
+}
+
+search.addEventListener('focusout', (e) => {
+    titles.style.display = (e.relatedTarget) ?
+        (((<HTMLElement>e.relatedTarget).classList[1] == "active") ? "":"none" ): "none"
+})
