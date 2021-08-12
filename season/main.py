22f14c6c8935939b57d64eb13d1b84d81969bce2
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2021-08-12 09:21:51 -0700

    Created a database for future season

---
+++ season/main.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from json import dump, load
+
+def main():
+    driver = webdriver.Firefox(service_log_path = '/dev/null',
+        firefox_profile = webdriver.FirefoxProfile("/home/dosx/.mozilla/firefox/eg34u3uo.default"))
+    driver.get('https://animeschedule.net/seasons/fall-2021')
+    data = driver.find_element_by_class_name('shows-container').find_elements_by_class_name('show-tile')
+    shows = {}
+    for show in data:
+        title = show.find_element_by_class_name('show-title').get_attribute('innerHTML')[1:-1]
+        cover = show.find_element_by_class_name('show-poster').get_attribute('src')
+        while "placeholder" in cover:
+            cover = show.find_element_by_class_name('show-poster').get_attribute('src')
+        studio = show.find_element_by_class_name('poster-information-studio').get_attribute('innerHTML')
+        source = show.find_element_by_class_name('poster-information-source').get_attribute('innerHTML')
+        genres = [ele.get_attribute('innerHTML') for ele in show.find_elements_by_class_name('genre')]
+        content = {
+            'cover': cover[:-12:],
+            'studio': studio,
+            'source': source,
+            'genres': genres
+            }
+        shows.update({title: content})
+    driver.quit()
+    with open('indent.json', 'w') as file:
+        dump(shows, file, indent = 4)
+    with open('shows.json', 'w') as file:
+        dump(shows, file, separators=(',', ':'))
+
+if __name__ == "__main__":
+    main()
