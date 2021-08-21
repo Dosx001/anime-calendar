86a341a50cc52dcdf8186a7218d5d5d07b760d59
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2021-08-20 17:04:31 -0700

    Improved overall responsiveness

--- js/calendar.js
+++ js/calendar.js
@@ -504,3 +504,10 @@ function updateTime() {
     }
     localStorage.setItem('time', JSON.stringify(now));
 }
+function menu() {
+    let menu = document.getElementById('menu');
+    menu.style.visibility = menu.style.visibility == 'visible' ? 'hidden' : 'visible';
+}
+window.matchMedia('(min-width: 800px)').addListener(_ => {
+    document.getElementById('menu').style.visibility = 'hidden';
+});
--- ts/calendar.ts
+++ ts/calendar.ts
@@ -542,3 +542,12 @@ function updateTime() {
     }
     localStorage.setItem('time', JSON.stringify(now))
 }
+
+function menu() {
+    let menu = document.getElementById('menu')!
+    menu.style.visibility = menu.style.visibility == 'visible' ? 'hidden' : 'visible'
+}
+
+window.matchMedia('(min-width: 800px)').addListener(_ => {
+    document.getElementById('menu')!.style.visibility = 'hidden'
+})
