f9c8141cdad3b24552ecc489175577fd1a2c22f9
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2021-12-07 14:08:38 -0800

    Automate getting FireFox profile

--- shows/main.py
+++ shows/main.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
 from shows import shows
 from sys import argv
+from os import popen
 
 def main():
+    profile = popen('ls ~/.mozilla/firefox/ | grep "default"').read().split("\n")[1]
+    user = popen("whoami").read()[0:-1]
+    path = "/home/" + user + "/.mozilla/firefox/" + profile
     sh = shows(webdriver.Firefox(service_log_path = '/dev/null',
-        firefox_profile = webdriver.FirefoxProfile("/home/dosx/.mozilla/firefox/eg34u3uo.default")))
+        firefox_profile = webdriver.FirefoxProfile(path)))
     sh.html()
     if len(argv) == 1:
         sh.update()
