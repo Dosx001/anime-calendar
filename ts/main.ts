7e306f1394390144dfe5a16442b6eba1ff4bb5d4
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2022-11-05 14:49:49 -0700

    Updated database: 22.3.10

--- ts/main.ts
+++ ts/main.ts
@@ -7,7 +7,7 @@
 document.getElementById('list')!.innerHTML =
   localStorage.getItem('list') ?? 'Your List';
 
-const CAL = new Calendar('22.3.10');
+const CAL = new Calendar('22.3.11');
 const SON = new Season();
 
 window.onload = () => {
