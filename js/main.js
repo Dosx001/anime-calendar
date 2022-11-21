c2d0ca8dda4e95f920badcfef41fe65aa1a03db1
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2022-11-21 12:05:30 -0800

    Updated database: 22.3.12

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.getElementById('info').selectedIndex = Number(localStorage.getItem('inf
 document.getElementById('format').selectedIndex = Number(localStorage.getItem('format'));
 document.getElementById('list').innerHTML =
     localStorage.getItem('list') ?? 'Your List';
-const CAL = new Calendar('22.3.11');
+const CAL = new Calendar('22.3.12');
 const SON = new Season();
 window.onload = () => {
     CAL.init().then(() => {
--- ts/main.ts
+++ ts/main.ts
@@ -7,7 +7,7 @@
 document.getElementById('list')!.innerHTML =
   localStorage.getItem('list') ?? 'Your List';
 
-const CAL = new Calendar('22.3.11');
+const CAL = new Calendar('22.3.12');
 const SON = new Season();
 
 window.onload = () => {
