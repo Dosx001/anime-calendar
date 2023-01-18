f2569882ab5bf0c94639ea22e70f551741ecd352
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-01-17 19:09:47 -0800

    Updated database: 23.0.2

--- ts/main.ts
+++ ts/main.ts
@@ -7,7 +7,7 @@
 document.getElementById('list')!.innerHTML =
   localStorage.getItem('list') ?? 'Your List';
 
-const CAL = new Calendar('23.0.2');
+const CAL = new Calendar('23.0.3');
 const SON = new Season();
 
 window.onload = () => {
