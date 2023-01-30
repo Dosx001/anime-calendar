fb3c937636c296399e4403ecc879a56a2957b149
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-01-29 16:44:48 -0800

    Updated gitignore

--- js/nav.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function menu() {
-    let menu = document.getElementById('menu');
-    menu.style.visibility = menu.style.visibility == 'visible' ? 'hidden' : 'visible';
-}
-window.matchMedia('(min-width: 800px)').addListener(_ => {
-    document.getElementById('menu').style.visibility = 'hidden';
-});
