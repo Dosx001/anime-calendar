c3bbc83dd03f705323467dca9fdaeabbfed85984
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-07-08 10:07:45 -0700

    chore(update): 23.2.1

--- ts/main.ts
+++ ts/main.ts
@@ -7,7 +7,7 @@
 document.getElementById('list')!.innerHTML =
   localStorage.getItem('list') ?? 'Your List';
 
-const CAL = new Calendar('23.2.0');
+const CAL = new Calendar('23.2.1');
 const SON = new Season();
 
 window.onload = () => {
